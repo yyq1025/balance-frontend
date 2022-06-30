45cb8f3b3512760e1dead94d15f1cd27bf6eefad
yyq321 <yangyq@umich.edu>
2022-06-29 23:25:06 -0400

    fix token expire

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { UserOutlined } from "@ant-design/icons";
 import { Row, Button, Space, Avatar, Typography } from "antd";
-import { useAppSelector } from "../../hooks";
-import { useNavigate, Link } from "react-router-dom";
+import { useAppSelector, useAppDispatch } from "../../hooks";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import jwt_decode, { JwtPayload } from "jwt-decode";
 const { Text } = Typography;
 
 const Navbar = () => {
   const navigate = useNavigate();
   const user = useAppSelector((state) => state.authData);
+
+  const dispatch = useAppDispatch();
+  const location = useLocation();
+
+  // Logout when token expired
+  React.useEffect(() => {
+    const token = user?.token;
+
+    if (token) {
+      const decodedToken = jwt_decode<JwtPayload>(token);
+
+      if (!decodedToken.exp || decodedToken.exp * 1000 < new Date().getTime())
+        dispatch({ type: "LOGOUT" });
+    }
+  }, [location]);
   return (
     <>
       <Row justify="space-between">
